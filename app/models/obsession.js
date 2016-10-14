a28bcf2da1779b930ca74301df3e2c2db04244e1
paulbhartzog <paulbhartzog@paulbhartzog.org>
2016-10-14 16:32:56 -0400

    made some changes

--- app/models/obsession.js
+++ app/models/obsession.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   body: DS.attr(),
+  link: DS.attr(),
   footer: DS.attr()
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,17 +2,44 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    let store = this.get('store');
 
-    this.get('store').createRecord('obsession', {
-      title: "Title 1",
-      body: "With supporting body below as a natural lead-in to additional content.",
-      footer: "footer"
-    });
-    this.get('store').createRecord('obsession', {
-      title: "Title 2",
-      body: "With supporting body below as a natural lead-in to additional content.",
-      footer: "footer"
-    });
+    let blog_posts = 3;
+    for(let i = 0; i < blog_posts; i++) {
+      let title = "Recent Post #" + i;
+      store.createRecord('obsession', {
+        title: title,
+        body: "blog post blog post blog post blog post blog post blog post blog post ",
+        link: "home",
+        footer: "footer"
+      });
+    }
+
+    let pages = ["futurist","code","music","lego","author"];
+    pages.forEach(function(item) {
+      let num = this.indexOf(item);
+      let title = "Title #" + num;
+      store.createRecord('obsession', {
+        title: title,
+        body: "Zog does stuff Zog does stuff Zog does stuff Zog does stuff Zog does stuff ",
+        link: item,
+        footer: "footer"
+      });
+    }, pages);
+
+/*
+    let pages = 6;
+    for(let i = 0; i < pages_length; i++) {
+      let num = i + 1;
+      let title = "Title #" + num;
+      store.createRecord('obsession', {
+        title: title,
+        body: "Zog does stuff Zog does stuff Zog does stuff Zog does stuff Zog does stuff ",
+        link: "home",
+        footer: "footer"
+      });
+    }
+*/
 
   }
 });
