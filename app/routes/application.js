3b0f927499455878d02a9565e57c4971546342cd
paulbhartzog <paulbhartzog@paulbhartzog.org>
2016-10-14 11:14:46 -0400

    changed push to createRecord

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,26 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
 
-    this.get('store').push({
-      data: [{
-        id: 1,
-        type: 'obsession',
-        attributes: {
-          title: "from push 1",
-          body: "With supporting body below as a natural lead-in to additional content.",
-          footer: "footer"
-        },
-        relationships: {}
-      }, {
-        id: 2,
-        type: 'obsession',
-        attributes: {
-          title: "from push 2",
-          body: "With supporting body below as a natural lead-in to additional content.",
-          footer: "footer"
-        },
-        relationships: {}
-      }]
+    this.get('store').createRecord('obsession', {
+      title: "from push 1",
+      body: "With supporting body below as a natural lead-in to additional content.",
+      footer: "footer"
+    });
+    this.get('store').createRecord('obsession', {
+      title: "from push 2",
+      body: "With supporting body below as a natural lead-in to additional content.",
+      footer: "footer"
     });
 
   }
